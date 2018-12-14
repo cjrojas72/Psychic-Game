4d08f87e54f1b6bba87b3a4558c50116befe2a2e
cjrojas72 <chris@Christians-MacBook-Pro.local>
2018-12-14 00:34:49 -0500

    update to game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,26 +23,45 @@ document.onkeyup = function(event) {
   // program alerts user if key pressed is not a letter
   if(!abc.includes(userKey)){
     console.log(userKey + " is not a letter!");
+    alert(`${userKey} is not a letter`);
   }
   // if user guesses letter righ then player gets point and game resets
   else if(userKey === programPick){
     console.log("Oh you guessed right!");
     wins++;
     guesses = 5;
+
+    
+    document.getElementById("wins-div").innerHTML = "Wins: " + wins; 
+
+    
     programPick = abc[Math.floor(abc.length * Math.random())]; 
-    console.log(programPick);
+    console.log("program chose: " + programPick);
   }
   // if user guesses letter wrong then player losses a guess until 
   else{
+    
+    var letters = document.createTextNode(`${userKey},`);
     console.log("Wrong!");
     guesses = guesses - 1;
+    document.getElementById("guesses-div").appendChild(letters);
+
+    document.getElementById("guesses-left").innerHTML = "Guesses Left: " + guesses; 
     console.log(guesses);
 
     if(guesses === 0){
+    
       console.log("You lose!");
+      losses++;
+      
+      document.getElementById("losses-div").innerHTML = "Losses: " + losses; 
+      
+      alert("Game over. Try Again?");
       guesses = 5;
+      document.getElementById("guesses-left").innerHTML = "Guesses Left: " + guesses; 
+      document.getElementById("guesses-div").innerHTML = "You Guessed: ";
       programPick = abc[Math.floor(abc.length * Math.random())]; 
-      console.log(programPick);
+      console.log("program chose: " + programPick);
     }
   }
 }
